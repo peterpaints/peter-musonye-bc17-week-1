2b2f934a861dc80b1b8c32f3f5f5644a024d3a1a
peter paints <musonyengigi@gmail.com>
2017-04-04 21:16:43 +0300

    Changed the function to implement a modified Sieve of Erastothenes for optimal performance.

--- day_1/gen_primes.py
+++ day_1/gen_primes.py
@@ -6,27 +6,27 @@ def gen_primes(n):
         raise ValueError("Only integers greater than 0 are allowed")
     else:
         """Generate prime numbers from 1 up to the specified number n."""
-        prime_numbers = []
-        for i in xrange(1, n + 1, 2):   # xrange, and skip all even numbers.
-            prime = True
-            # This filters out non-primes. A prime number is not divisible
-            # by any number other than itself
-            for j in xrange(2, i - 1):
-                if i % j == 0:
-                    prime = False
-            if prime:   # if prime is not false.
+        # Modified this function to implement a sieve of erastothenes.
+        prime_numbers = [2] # let's have 2 here, so that we can skip all even numbers in the outer loop
+        sieve = [True] * (n + 1)
+        for i in xrange(3, n + 1, 2):   # xrange, and skip all even numbers.
+            if sieve[i]:
                 prime_numbers.append(i)
+                # we now want to cross out multiples of all i's that are primes, (hence the i at the end of range() to skip them).
+                # we start at squares of i since anything below that has already been
+                # eliminated by smaller i's.
+                for j in range(i**2, n + 1, i): # for some reason, xrange here returns a StackOverflow error with
+                    sieve[j] = False            # large ints like 1000000
         return prime_numbers
 
-# test case n
-# print
-gen_primes(1000000)
+
+gen_primes(10000)
 
 import time
 start_time = time.time()
 # main()
 print("--- %s seconds ---" % (time.time() - start_time))
 
-# gen_primes(39) logs 0.00000214576721191 seconds
-# gen_primes(10000) logs 0.00000309944152832 seconds
-# gen_primes(1000000) did not finish.
+# gen_primes(39) now logs 0.00000190734863281 seconds.
+# gen_primes(10000) now logs 0.000000953674316406 seconds.
+# gen_primes(1000000) now logs 0.00000286102294922 seconds.
