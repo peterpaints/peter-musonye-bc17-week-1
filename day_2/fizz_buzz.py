1dcf1d27b86ebe2980e3fa40292335efc3e343a4
peter paints <musonyengigi@gmail.com>
2017-04-04 14:17:13 +0300

    Cleaned up duplicate files

--- day_2/fizz_buzz.py
+++
@@ -1,9 +0,0 @@
-def fizz_buzz(i):
-    if i % 3 == 0 and i % 5 == 0:
-        return "FizzBuzz"
-    elif i % 3 == 0:
-        return "Fizz"
-    elif i % 5 == 0:
-        return "Buzz"
-    else:
-        return i
