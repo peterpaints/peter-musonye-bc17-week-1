c9a9c6a14b8f6d3826880e723fe870ed086c5cf6
peter paints <musonyengigi@gmail.com>
2017-04-08 17:51:53 +0300

    Added other solutions as comments.

--- day_4/missing_num/missing_num.py
+++ day_4/missing_num/missing_num.py
@@ -5,6 +5,22 @@ def find_missing(A, B):
         result ^= number  # XOR means 'strictly or', remember binary operators?
 
     return result   # or as a one liner; return reduce(lambda x, y: x^y, C)
+    # if A == B:
+    #     return 0
+    # for num in C:
+    #     if num in A and num in B:
+    #         pass
+    #     else:
+            # result.append(num)
+    #         return num
+    # return result
+    # for num in C:
+    #     if C.count(num) % 2 != 0:
+    #         # return num
+    #         if num not in result:
+    #             result.append(num)
+    # return result
 
 
+# print find_missing([9, 9, 7, 6, 5, 3, 4, 2, 1], [9, 7, 6, 5, 3, 4, 2, 1, 8])
 # print find_missing([2], [2])
